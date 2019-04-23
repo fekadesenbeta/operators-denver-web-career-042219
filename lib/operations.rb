1b03582ac6714a43bac97f33b3c16c36987008fc
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-23 00:03:56 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,10 @@
 def unsafe?(speed)
- if speed < 40 || > 60
-   return true
-else 
-    return false
-
+if speed > 60 || speed < 40
+	return true
+else
+	return false
 end
-
+end
 
 
 def not_safe?(speed)
